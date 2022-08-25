e48ae11b146c97eaca915a4237d0ce2076194ba9
Ola Jeppsson <ola.jeppsson@gmail.com>
2022-08-25 12:32:04 +0200

    poky & prepareyoctobuild.sh: Add required patch
    We need this backported patch to be able to have epiphany as a second
    architecture. We can remove this when we update to a newer Yocto
    release.
    
    Patch:
    sstate: do not add TARGET_ARCH to pkgarch for cross recipes.
    
    Signed-off-by: Ola Jeppsson <ola.jeppsson@gmail.com>

--- prepareyoctobuild.sh
+++ prepareyoctobuild.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Need this backported patch to be able to have epiphany as a second
+# architecture. We can remove this when we update to a newer Yocto release.
+(cd poky; git am ../developer-zone/0001-backport-sstate-do-not-add-TARGET_ARCH-to-pkgarch-fo.patch >/dev/null 2>&1 || git am --abort >/dev/null 2>&1)
+
 # Create the build folder
 source poky/oe-init-build-env build_parallella
 
